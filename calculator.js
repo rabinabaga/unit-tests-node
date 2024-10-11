e5b2439d70f94f9021367fd567629f94badeac9b
rabinabaga <rabinabaga@gmail.com>
2024-10-11 12:42:34 +0545

    jest supports common js by default

--- calculator.js
+++ calculator.js
@@ -1,8 +1,13 @@
 function add(numbers) {
+  if (!numbers) return 0; // Return 0 for empty string
   return numbers
     .split(",")
     .map((x) => parseInt(x))
-    .reduce((a, b) => a + b );
+    .reduce((a, b) => a + b, 0); // Start with 0 to handle single element properly
 }
 
-module.exports = { add };
+function hello(){
+  return "Hello"
+}
+
+module.exports = {add, hello}; 
